416ee37b5f6fafc6b17afa60fb34f112422bb7ef
Aline Santana <aline_santana13@hotmail.com>
2024-03-24 01:37:37 -0300

    Ajuste no layout do projeto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { CurriculumComponent } from './pages/curriculum/curriculum.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'curriculum', component: CurriculumComponent},
+  {path: 'projects', component: ProjectsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,8 +6,10 @@ import { AppComponent } from './app.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { HomeComponent } from './pages/home/home.component';
-import { PresentationComponent } from './components/presentation/presentation.component';
 import { TechnologiesComponent } from './components/technologies/technologies.component';
+import { AboutComponent } from './pages/about/about.component';
+import { CurriculumComponent } from './pages/curriculum/curriculum.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
 
 @NgModule({
   declarations: [
@@ -15,12 +17,14 @@ import { TechnologiesComponent } from './components/technologies/technologies.co
     FooterComponent,
     NavBarComponent,
     HomeComponent,
-    PresentationComponent,
-    TechnologiesComponent
+    TechnologiesComponent,
+    AboutComponent,
+    CurriculumComponent,
+    ProjectsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
